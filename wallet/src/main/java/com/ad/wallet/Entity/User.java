319d698621dde0e44db1509780f21ef98a9596ee
bhanuteja449896 <bhanutejamakkineni@gmail.com>
2025-01-26 00:54:02 +0530

    first

---
+++ wallet/src/main/java/com/ad/wallet/Entity/User.java
@@ -0,0 +1,108 @@
+package com.ad.wallet.Entity;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private String mobile;
+    private String email;
+    private String balance;
+    private List<Transaction> transactions;
+
+    public static class Transaction {
+        private Double amount;
+        private String status;
+        private String timestamp;
+        private String type; // "credit" or "debit"
+        private String receiver;
+        private String sender;
+
+        public Double getAmount() {
+            return amount;
+        }
+
+        public void setAmount(Double amount) {
+            this.amount = amount;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public String getTimestamp() {
+            return timestamp;
+        }
+
+        public void setTimestamp(String timestamp) {
+            this.timestamp = timestamp;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getReceiver() {
+            return receiver;
+        }
+
+        public void setReceiver(String receiver) {
+            this.receiver = receiver;
+        }
+
+        public String getSender() {
+            return sender;
+        }
+
+        public void setSender(String sender) {
+            this.sender = sender;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+}
---
+++ wallet/src/main/java/com/ad/wallet/WalletApplication.java
@@ -0,0 +1,15 @@
+package com.ad.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(WalletApplication.class, args);
+		System.out.println("running on port 8080");
+		
+	}
+}
